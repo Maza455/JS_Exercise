800583dbd0f44a23f10f9d81fbc1345e62b69d90
Maza455 <same7nte@gmail.com>
2023-10-10 15:25:06 +0200

    BasicJs

---
+++ script/code.js
@@ -0,0 +1,8 @@
+let hours = prompt("Enter your hours: ");
+let rate = prompt("Enter your rate: ");
+
+let salary = hours * rate;
+
+// document.getElementById(salary);
+
+console.log(`Your salary is: R${salary}`);
